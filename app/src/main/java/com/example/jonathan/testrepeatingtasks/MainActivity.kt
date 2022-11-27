804f0443640f4c3932493ebaf91c02380181131a
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2022-11-27 11:56:02 -0800

    Clean up boilter-plate code; added MainActivity lifecycle logging.

--- app/src/main/java/com/example/jonathan/testrepeatingtasks/MainActivity.kt
+++ app/src/main/java/com/example/jonathan/testrepeatingtasks/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.jonathan.testrepeatingtasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "onStart")
+
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume")
+
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+
+        super.onDestroy()
+    }
+
+    companion object {
+        private const val TAG = "TRPT: MainActivity"
     }
 }
