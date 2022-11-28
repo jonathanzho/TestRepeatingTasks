d5153e250b90d45440d65a671c0575ce9c630d37
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2022-11-27 20:59:37 -0800

    implemented a repeating task that runs every UPDATE_INTERVAL milliseconds when MainActivity is on the foreground.

--- app/src/main/java/com/example/jonathan/testrepeatingtasks/MainActivity.kt
+++ app/src/main/java/com/example/jonathan/testrepeatingtasks/MainActivity.kt
@@ -2,14 +2,35 @@ package com.example.jonathan.testrepeatingtasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var mTextView: TextView? = null
+
+    private val mHandler = Handler(Looper.getMainLooper())
+
+    private val mRunnable = object : Runnable {
+        override fun run() {
+            runCount++
+
+            Log.d(TAG, "mRunnable: runCount=$runCount")
+
+            mTextView?.text = "runCount=$runCount"
+
+            mHandler.postDelayed(this, UPDATE_INTERVAL)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate")
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mTextView = findViewById(R.id.run_count_text_view)
     }
 
     override fun onStart() {
@@ -22,12 +43,16 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onResume")
 
         super.onResume()
+
+        startRepeatingTask()
     }
 
     override fun onPause() {
         Log.d(TAG, "onPause")
 
         super.onPause()
+
+        stopRepeatingTask()
     }
 
     override fun onStop() {
@@ -42,7 +67,23 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
+    private fun startRepeatingTask() {
+        Log.d(TAG, "startRepeatingTask")
+
+        mRunnable.run()
+    }
+
+    private fun stopRepeatingTask() {
+        Log.d(TAG, "stopRepeatingTask")
+
+        mHandler.removeCallbacks(mRunnable)
+    }
+
     companion object {
         private const val TAG = "TRPT: MainActivity"
+
+        private const val UPDATE_INTERVAL = 5000L
+
+        private var runCount = 0
     }
 }
